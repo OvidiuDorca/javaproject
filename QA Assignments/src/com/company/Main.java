cf483a381889f6001133b628429bd1cb80dd36cc
OvidiuDorca <ovidiu.dorca@yahoo.com>
2019-05-30 11:49:26 +0300

    Initial commit

---
+++ QA Assignments/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.io.OutputStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    System.out.println("hello");
+    System.out.println("Ovidiu");
+    System.out.println(2+6);
+    System.out.println(-5+8*6);
+    System.out.println((55+9)%9);
+    System.out.println(20+-3*5);
+    System.out.println(5+15/3*-8%3);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
